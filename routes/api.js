079d954020937eaf5d2c7989ea63918934c729f6
Santiago Olguín <SantoII1735@gmail.com>
2024-09-24 15:08:19 +0000

    added 'api/convert' route

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,23 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+  
+  app.get('/api/convert', (req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
 
+    if (initNum === undefined && initUnit === undefined) {
+      res.json("invalid number and unit");
+    } else if (initNum === undefined) {
+      res.json("invalid number");
+    } else if (initUnit === undefined) {
+      res.json("invalid unit");
+    } else {
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      res.json({ initNum, initUnit, returnNum, returnUnit, string });
+    }
+  });
 };
