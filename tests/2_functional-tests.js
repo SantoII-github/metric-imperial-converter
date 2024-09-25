9dd01b434f07e7394872455749325b9514ee13a5
Santiago Olguín <SantoII1735@gmail.com>
2024-09-25 15:30:30 +0000

    finished functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,74 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+    suite("Routing Tests", function() {
+        suite("GET /api/convert => conversion object", function () {
+            test("Convert 10L (valid input)", function(done) {
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({ input: "10L" })
+                    .end((err, res) => {
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.initNum, 10);
+                        assert.equal(res.body.initUnit, "L");
+                        assert.approximately(res.body.returnNum, 2.64172, 0.1);
+                        assert.equal(res.body.returnUnit, "gal");
+                        done();
+                    });
+            });
 
+            test("Convert 32g (invalid input unit)", function(done) {
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({ input: "32g" })
+                    .end((err, res) => {
+                        assert.equal(res.status, 200);
+                        assert.isUndefined(res.body.initUnit);
+                        done();
+                    });
+            });
+
+            test("Convert 3/7.2/4kg (invalid input number)", function(done) {
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({ input: "3/7.2/4kg" })
+                    .end((err, res) => {
+                        assert.equal(res.status, 200);
+                        assert.isUndefined(res.body.initNum);
+                        done();
+                    });
+            });
+
+            test("Convert 3/7.2/4kilomegagram (invalid input number and unit)", function(done) {
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({ input: "3/7.2/4kilomegagram" })
+                    .end((err, res) => {
+                        assert.equal(res.status, 200);
+                        assert.isUndefined(res.body.initNum);
+                        assert.isUndefined(res.body.initUnit);
+                        done();
+                    });
+            });
+
+            test("Convert kg (valid input no number)", function(done) {
+                chai
+                    .request(server)
+                    .get('/api/convert')
+                    .query({ input: "kg" })
+                    .end((err, res) => {
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.initNum, 1);
+                        assert.equal(res.body.initUnit, "kg");
+                        assert.approximately(res.body.returnNum, 2.20462, 0.1);
+                        assert.equal(res.body.returnUnit, "lbs");
+                        done();
+                    });
+            });
+        });
+    });
 });
